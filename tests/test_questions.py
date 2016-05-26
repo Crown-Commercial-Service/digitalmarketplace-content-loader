1c7946b828b96f8eb0c6a3b25344275398bf0286
Alexey Bezhan <al@fueledbylemons.com>
2016-05-26 15:48:35 +0100

    Add helper .question methods for content question tests

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -3,68 +3,52 @@
 from dmcontent.content_loader import ContentQuestion
 
 
-class TestBaseQuestion(object):
-    def test_form_fields_property(self):
-        question = ContentQuestion({
+class TestText(object):
+    def question(self, **kwargs):
+        data = {
             "id": "example",
             "type": "text"
-        })
-        assert question.form_fields == ['example']
+        }
+        data.update(kwargs)
+
+        return ContentQuestion(data)
+
+    def test_form_fields_property(self):
+        assert self.question().form_fields == ['example']
 
 
 class TestPricing(object):
-    def test_form_fields(self):
-        question = ContentQuestion({
+    def question(self, **kwargs):
+        data = {
             "id": "example",
             "type": "pricing",
             "fields": {
                 "minimum_price": "priceMin",
                 "maximum_price": "priceMax",
             }
-        })
-        assert sorted(question.form_fields) == sorted(['priceMin', 'priceMax'])
+        }
+        data.update(kwargs)
+
+        return ContentQuestion(data)
+
+    def test_form_fields(self):
+        assert sorted(self.question().form_fields) == sorted(['priceMin', 'priceMax'])
 
     def test_required_form_fields_property(self):
-        question = ContentQuestion({
-            "id": "example",
-            "type": "pricing",
-            "fields": {
-                "minimum_price": "priceMin",
-                "maximum_price": "priceMax",
-            }
-        })
-        assert sorted(question.required_form_fields) == sorted(['priceMin', 'priceMax'])
+        assert sorted(self.question().required_form_fields) == sorted(['priceMin', 'priceMax'])
 
     def test_required_form_fields_property_when_optional(self):
-        question = ContentQuestion({
-            "id": "example",
-            "type": "pricing",
-            "optional": True,
-            "fields": {
-                "minimum_price": "priceMin",
-                "maximum_price": "priceMax",
-            }
-        })
+        question = self.question(optional=True)
         assert question.required_form_fields == []
 
     def test_required_form_fields_property_with_optional_fields(self):
-        question = ContentQuestion({
-            "id": "example",
-            "type": "pricing",
-            "fields": {
-                "minimum_price": "priceMin",
-                "maximum_price": "priceMax",
-            },
-            "optional_fields": [
-                "minimum_price"
-            ]
-        })
+        question = self.question(optional_fields=["minimum_price"])
         assert question.required_form_fields == ['priceMax']
 
 
 class TestMultiquestion(object):
-    def test_form_fields(self):
-        question = ContentQuestion({
+    def question(self, **kwargs):
+        data = {
             "id": "example",
             "type": "multiquestion",
             "questions": [
@@ -77,24 +61,25 @@ class TestMultiquestion(object):
                     "type": "text",
                 }
             ]
-        })
-        assert question.form_fields == ['example2', 'example3']
+        }
+        data.update(kwargs)
+
+        return ContentQuestion(data)
+
+    def test_form_fields(self):
+        assert self.question().form_fields == ['example2', 'example3']
 
     def test_required_form_fields(self):
-        question = ContentQuestion({
-            "id": "example",
-            "type": "multiquestion",
-            "questions": [
-                {
-                    "id": "example2",
-                    "type": "text",
-                    "optional": False,
-                },
-                {
-                    "id": "example3",
-                    "type": "text",
-                    "optional": True,
-                }
-            ]
-        })
+        question = self.question(questions=[
+            {
+                "id": "example2",
+                "type": "text",
+                "optional": False,
+            },
+            {
+                "id": "example3",
+                "type": "text",
+                "optional": True,
+            }
+        ])
         assert question.required_form_fields == ['example2']
