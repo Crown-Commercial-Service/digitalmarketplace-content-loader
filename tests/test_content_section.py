4612ecca6e6b7e03261e765f1d83f7adc2824290
Paul Craig <paul_craig_16@hotmail.com>
2016-10-27 11:55:18 +0100

    Test `_filtered` attribute is preserved
    While testing with the frontend apps, we found that the `_filtered`
    attribute wasn't being copied correctly or set correctly when
    returning a multiquestion as a section.
    We've since fixed the problem, and now these tests verify that
    everything works.

--- tests/test_content_section.py
+++ tests/test_content_section.py
@@ -1,7 +1,7 @@
 import pytest
 from jinja2 import UndefinedError
 
-from dmcontent.questions import Question
+from dmcontent.questions import Question, Multiquestion
 from dmcontent.content_loader import ContentSection
 
 
@@ -99,7 +99,7 @@ class TestFilterContentSection(object):
 
         assert section.filter({}).description is None
 
-    def test_get_section_description_before_filter_raises_error(self):
+    def test_get_templatable_section_attributes_before_filter_raises_error(self):
         section = ContentSection(
             slug='section',
             name='Section one',
@@ -110,7 +110,54 @@ class TestFilterContentSection(object):
         )
 
         with pytest.raises(TypeError):
+            section.name
+
+        with pytest.raises(TypeError):
             section.description
+
+        assert section.slug == 'section'
+
+    def test_copying_section_preserves_value_of_filtered_attribute(self):
+        section = ContentSection(
+            slug='section',
+            name='Section',
+            editable=False,
+            edit_questions=False,
+            questions=[Question({'name': 'Question'})]
+        ).filter({})
+        assert section._filtered
+
+        copied_section = section.copy()
+        assert copied_section._filtered
+
+    def test_getting_a_multiquestion_as_a_section_preserves_value_of_filtered_attribute(self):
+        multiquestion_data = {
+            "id": "example",
+            "slug": "example",
+            "question": "Example question",
+            "type": "multiquestion",
+            "questions": [
+                {
+                    "id": "example2",
+                    "type": "text",
+                },
+                {
+                    "id": "example3",
+                    "type": "number",
+                }
+            ]
+        }
+
+        section = ContentSection(
+            slug='section',
+            name='Section',
+            editable=False,
+            edit_questions=False,
+            questions=[Multiquestion(multiquestion_data)]
+        )
+
+        assert not section.get_question_as_section('example')._filtered
+        assert section.filter({}).get_question_as_section('example')._filtered
 
     def test_filtering_a_section_with_a_description_template(self):
         section = ContentSection(
