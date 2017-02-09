1fba74dad93d16abaff356afc81102bea4b8d7eb
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2017-02-09 08:20:33 +0000

    Bump version to 2.6.0

--- dmcontent/__init__.py
+++ dmcontent/__init__.py
@@ -2,4 +2,4 @@ from .content_loader import ContentLoader
 from .errors import ContentTemplateError, QuestionNotFoundError
 from .questions import ContentQuestion
 
-__version__ = '2.5.0'
+__version__ = '2.6.0'
