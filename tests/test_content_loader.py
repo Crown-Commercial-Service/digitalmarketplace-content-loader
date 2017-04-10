08191f91ac6df3f855024d577216ef86e6720d32
benvand <benjaminfvandersteen@gmail.com>
2017-04-10 17:39:26 +0100

    BUGFIX-failing-YAML-test
    digitalmarketplace-content-loader/tests/test_content_loader.py::TestReadYaml::test_loading_existant_file
    The above was failing on master, there's been a refactor recently but
    not sure what caused it tbh.
    Fixed mock to point at instance open that is used by the function being
    tested.

--- tests/test_content_loader.py
+++ tests/test_content_loader.py
@@ -1699,11 +1699,11 @@ class TestContentSection(object):
 
 
 class TestReadYaml(object):
-    @mock.patch.object(builtins, 'open', return_value=io.StringIO(u'foo: bar'))
+    @ mock.patch('dmcontent.content_loader.open', return_value=io.StringIO(u'foo: bar'))
     def test_loading_existant_file(self, mocked_open):
         assert read_yaml('anything.yml') == {'foo': 'bar'}
 
-    @mock.patch.object(builtins, 'open', side_effect=IOError)
+    @ mock.patch('dmcontent.content_loader.open', side_effect=IOError)
     def test_file_not_found(self, mocked_open):
         with pytest.raises(IOError):
             assert read_yaml('something.yml')
