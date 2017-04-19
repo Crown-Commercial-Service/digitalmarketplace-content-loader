c5dae54b2470d93c26ca586b4dd06c0f7f09c87c
benvand <benjaminfvandersteen@gmail.com>
2017-04-19 13:07:31 +0100

    Version bump to 4.0.0 and changelog description.

--- dmcontent/__init__.py
+++ dmcontent/__init__.py
@@ -2,4 +2,4 @@ from .content_loader import ContentLoader
 from .errors import ContentTemplateError, QuestionNotFoundError
 from .questions import ContentQuestion
 
-__version__ = '3.7.0'
+__version__ = '4.0.0'
