a90ad3a10c274d18b1455e7501e8e5dc067ab4d9
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2018-06-19 17:51:02 +0100

    Add utils `try_load_messages` and `try_load_metadata`
    We already have one for handling exceptions when loading manifests. It'd
    be helpful to helpers for loading metadata and messages too.

--- dmcontent/__init__.py
+++ dmcontent/__init__.py
@@ -2,4 +2,4 @@ from .content_loader import ContentLoader
 from .errors import ContentTemplateError, QuestionNotFoundError
 from .questions import ContentQuestion
 
-__version__ = '4.10.1'
+__version__ = '4.11.0'
