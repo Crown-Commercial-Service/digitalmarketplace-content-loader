710760bfc44610305338097300312b39698032d5
benvand <benjaminfvandersteen@gmail.com>
2018-09-10 12:51:59 +0100

    Version bump and update changelog for breaking change

--- dmcontent/__init__.py
+++ dmcontent/__init__.py
@@ -2,4 +2,4 @@ from .content_loader import ContentLoader
 from .errors import ContentTemplateError, QuestionNotFoundError
 from .questions import ContentQuestion
 
-__version__ = '4.11.0'
+__version__ = '5.0.0'
