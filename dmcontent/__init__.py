11b16ad4c46bee7cb12d10116983c7767655fe50
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-03-07 12:29:37 +0000

    Bump version (minor) to 5.2.0

--- dmcontent/__init__.py
+++ dmcontent/__init__.py
@@ -2,4 +2,4 @@ from .content_loader import ContentLoader
 from .errors import ContentTemplateError, QuestionNotFoundError
 from .questions import ContentQuestion
 
-__version__ = '5.1.3'
+__version__ = '5.2.0'
