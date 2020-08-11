5835d549b0bb0fd2c1d32a0022f45ba3c520f8d6
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-08-11 11:38:22 +0100

    Add dmcontent.govuk_frontend._params
    There are a lot of parameters in govuk-frontend macros such as
    `govukInput`, and a lot of them are consistently named and typed between
    different macros.
    
    This commit abstracts most of these into a function called `_params`.
    The name is prefixed with an underscore and not exported in __all__
    because it will probably change a lot over time (for instance, not
    every macro uses a label, so at some point there will need to be a
    function `_label` splitting out that behaviour).
    
    However, the advantage is that we can have a comprehensive test suite
    for this fairly fiddly logic.

---
+++ dmcontent/govuk_frontend.py
@@ -0,0 +1,92 @@
+"""
+Create forms to answer questions using govuk-frontend macros.
+"""
+
+from typing import Optional
+
+from jinja2 import Markup, escape
+
+from dmutils.forms.errors import govuk_error
+
+from dmcontent.questions import Question
+
+
+__all__ = []
+
+
+def _params(
+    question: Question, data: Optional[dict] = None, errors: Optional[dict] = None
+) -> dict:
+    """Common parameters for govuk-frontent components
+
+    The GOV.UK Design System macro library govuk-frontend has a consistent set
+    of parameters that are used across almost all of its component macros.
+
+    This function abstracts out those common parameters to hopefully simplify
+    creating parameter sets for specific macros.
+
+    *This function should however be considered an implementation detail of
+    this module, and you should avoid using it outside of this file (hence the
+    underscore).*
+
+    The parameters handled by this function include:
+
+        - errorMessage (optional)
+        - hint (optional)
+        - id
+        - label
+        - name
+        - value (optional)
+
+    :returns: A dictionary with parameters that are generally useful for
+              govuk-frontend component macros
+    """
+    params = {
+        "id": f"input-{question.id}",
+        "name": question.id,
+    }
+
+    label_text = question.question
+    if question.is_optional:
+        # GOV.UK Design System says
+        # > mark the labels of optional fields with '(optional)'
+        label_text += " (optional)"
+
+    params["label"] = {
+        # Style the label as a page heading, following the
+        # GOV.UK Design System question pages pattern at
+        # https://design-system.service.gov.uk/patterns/question-pages/
+        "classes": "govuk-label--l",
+        "isPageHeading": True,
+
+        "text": label_text,
+    }
+
+    hint_html = Markup()
+    if question.get("question_advice"):
+        # Put the question advice inside the hint, wrapped in a div
+        # We add the class .app-hint--text so the question advice
+        # can be styled like a normal paragraph with the following Sass
+        #
+        #     .app-hint--text {
+        #       @extend %govuk-body--m
+        #     }
+        hint_html += Markup('<div class="app-hint--text">\n')
+        hint_html += escape(question.question_advice)
+        hint_html += Markup("\n</div>")
+        if question.get("hint"):
+            hint_html += "\n"
+
+    if question.get("hint"):
+        hint_html += escape(question.hint)
+
+    if hint_html:
+        params["hint"] = {"html": hint_html}
+
+    if data and question.id in data:
+        params["value"] = data[question.id]
+
+    if errors and question.id in errors:
+        params["errorMessage"] = govuk_error(errors[question.id])["errorMessage"]
+
+    return params
---
+++ tests/test_govuk_frontend.py
@@ -0,0 +1,129 @@
+import pytest
+
+from jinja2 import Markup
+
+from dmcontent.questions import Question
+
+from dmcontent.govuk_frontend import _params
+
+
+class TestParams:
+    @pytest.fixture
+    def question(self):
+        return Question({"id": "question", "question": "Yes or no?"})
+
+    def test_govuk_input(self, question):
+        assert _params(question) == {
+            "id": "input-question",
+            "name": "question",
+            "label": {
+                "classes": "govuk-label--l",
+                "isPageHeading": True,
+                "text": "Yes or no?",
+            },
+        }
+
+    def test_hint(self, question):
+        question.hint = "Answer yes or no"
+
+        assert _params(question)["hint"] == {
+            "html": "Answer yes or no",
+        }
+
+    def test_hint_is_escaped(self, question):
+        question.hint = "<script>"
+
+        assert _params(question)["hint"]["html"] == "&lt;script&gt;"
+
+    def test_question_advice_is_part_of_hint(self, question):
+        question.question_advice = "You should answer yes or no."
+
+        hint = _params(question)["hint"]
+
+        assert hint == {
+            "html": '<div class="app-hint--text">\n'
+            "You should answer yes or no.\n"
+            "</div>"
+        }
+        assert isinstance(hint["html"], Markup)
+
+    def test_question_advice_is_escaped(self, question):
+        question.question_advice = "<script>"
+
+        assert _params(question)["hint"] == {
+            "html": '<div class="app-hint--text">\n' "&lt;script&gt;\n" "</div>"
+        }
+
+    def test_question_advice_and_hint(self, question):
+        question.hint = "Answer yes or no"
+        question.question_advice = "You should answer yes or no."
+
+        assert _params(question)["hint"] == {
+            "html": '<div class="app-hint--text">\n'
+            "You should answer yes or no.\n"
+            "</div>\n"
+            "Answer yes or no"
+        }
+
+    def test_optional_question_has_optional_in_label_text(self, question):
+        question.optional = True
+
+        assert _params(question)["label"]["text"] == "Yes or no? (optional)"
+
+    def test_not_optional_question_does_not_have_optional_in_label_text(self, question):
+        question.optional = False
+
+        assert _params(question)["label"]["text"] == "Yes or no?"
+
+    def test_value_is_present_if_question_answer_is_in_data(self, question):
+        data = {"question": "Yes"}
+
+        assert _params(question, data)["value"] == "Yes"
+
+    def test_value_is_not_present_if_question_answer_is_not_in_data(self, question):
+        data = {"another_question": "Maybe"}
+
+        assert "value" not in _params(question, data)
+
+    def test_error_message_is_present_if_question_error_is_in_errors(self, question):
+        errors = {
+            "question": {
+                "input_name": "question",
+                "href": "#input-question",
+                "question": "Yes or no?",
+                "message": "Answer yes or no.",
+            }
+        }
+
+        assert _params(question, errors=errors)["errorMessage"] == {
+            "text": "Answer yes or no."
+        }
+
+    def test_error_message_is_not_present_if_question_error_is_not_in_errors(
+        self, question
+    ):
+        errors = {
+            "another_question": {
+                "input_name": "another-question",
+                "href": "#input-another-question",
+                "question": "Are you sure?",
+                "message": "Enter whether you are sure or not.",
+            }
+        }
+
+        assert "errorMessage" not in _params(question, errors=errors)
+
+    def test_value_and_error_message(self, question):
+        data = {"question": "Definitely"}
+        errors = {
+            "question": {
+                "input_name": "question",
+                "href": "#input-question",
+                "question": "Yes or no?",
+                "message": "Answer yes or no only.",
+            }
+        }
+
+        params = _params(question, data, errors)
+        assert params["value"] == "Definitely"
+        assert params["errorMessage"]["text"] == "Answer yes or no only."
