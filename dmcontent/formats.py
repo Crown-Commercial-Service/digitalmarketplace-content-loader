09815e2fbf8d25ea5b0fbc63ed58291b3b1954d5
Alex Smith <alex.smith@digital.cabinet-office.gov.uk>
2020-10-20 08:48:35 +0100

    Add commas to prices greater than £999

--- dmcontent/formats.py
+++ dmcontent/formats.py
@@ -2,16 +2,40 @@
 from typing import Optional, Union
 
 
+def comma_format(number: Union[str, float, int]) -> str:
+    """
+    Format a number with commas if it's greater than 999.
+    eg: `1000 -> '1,000'
+        123456.78 -> '123,456.78'
+        '1000000' -> '1,000,000'
+        500 -> '500'
+
+    :param number: The number to be formatted. Either an int, a float or a str.
+    :return: A string of the comma formatted number
+    """
+    if type(number) == str:
+        try:
+            number = int(number)
+        except ValueError:
+            number = float(number)
+
+    if type(number) == int:
+        # Don't need to add decimal places to an int
+        return f"{number:,}"
+    else:
+        return f"{number:,.2f}"
+
+
 def format_price(min_price: Optional[Union[str, float]],
                  max_price: Optional[Union[str, float]],
                  unit: Optional[str],
                  interval: Optional[str],
                  hours_for_price: Optional[str] = None):
     """Format a price string"""
-    if min_price is not None:
-        min_price = str(min_price)
-    if max_price is not None:
-        max_price = str(max_price)
+    if min_price is not None and min_price != '':
+        min_price = comma_format(min_price)
+    if max_price is not None and max_price != '':
+        max_price = comma_format(max_price)
 
     if not (min_price or max_price):
         raise TypeError('One of min_price or max_price should be number or non-empty string, not None')
